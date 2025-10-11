80222dc7d9abe4e52b1f7ba7052f8cfee8877ed2
Islam Elshaikh <islamellshaikh22@gmail.com>
2025-10-11 17:47:53 +0200

    add class based view

--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 # from django.conf.urls import handler404
 from . import views
 urlpatterns = [
-    path('',views.starting_page, name='starting-page'),
-    path('posts',views.posts, name='posts-page'),
-    path('posts/<slug:slug>',views.single_post,name='post-detail-page'),
+    path('', views.StartingPageView.as_view(), name='starting-page'),
+    #path('',views.starting_page, name='starting-page'),
+    path('posts',views.AllPostView.as_view(), name='posts-page'),
+    #path('posts',views.posts, name='posts-page'),
+    path('posts/<slug:slug>',views.SinglePostView.as_view(),name='post-detail-page'),
+    # path('posts/<slug:slug>',views.single_post,name='post-detail-page'),
 ]
 # handler404='blog.views.custom_404'
--- blog/views.py
+++ blog/views.py
@@ -1,21 +1,53 @@
 from django.shortcuts import render, get_object_or_404
-
+from django.views.generic import ListView , DeleteView
 from .models import Post
 # Create your views here.
 
-def starting_page(request):
-    all_posts = Post.objects.all().order_by('-date')[:3]
-    return render(request,'blog/index.html',{
-        'posts':all_posts
-    })
+#class of starting page
+class StartingPageView(ListView):
+    template_name = 'blog/index.html'
+    model = Post
+    context_object_name = 'posts'
+    ordering = ["-date"]
 
-def posts(request):
-    all_posts = Post.objects.all().order_by('-date')
-    return render(request,'blog/all-posts.html',{'posts':all_posts})
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        data = queryset[:3]
+        return data
 
-def single_post(request, slug):
-    post = get_object_or_404(Post, slug = slug)
-    return render(request, 'blog/post-detail.html',{'post':post, 'post_tages':post.tag.all()})
+
+# def starting_page(request):
+#     all_posts = Post.objects.all().order_by('-date')[:3]
+#     return render(request,'blog/index.html',{
+#         'posts':all_posts
+#     })
+
+#class of all post
+class AllPostView(ListView):
+    template_name = 'blog/all-posts.html'
+    model = Post
+    context_object_name = 'posts'
+    ordering = ['-date']
+
+
+# def posts(request):
+#     all_posts = Post.objects.all().order_by('-date')
+#     return render(request,'blog/all-posts.html',{'posts':all_posts})
+
+class SinglePostView(DeleteView):
+    template_name = 'blog/post-detail.html'
+    model = Post
+    slug_field = 'slug'
+
+    def get_context_data(self, **kwargs):
+        context =  super().get_context_data(**kwargs)
+        context['post_tages'] = self.object.tag.all()
+        return context
+    
+
+# def single_post(request, slug):
+#     post = get_object_or_404(Post, slug = slug)
+#     return render(request, 'blog/post-detail.html',{'post':post, 'post_tages':post.tag.all()})
 
 
 # def custom_404(request, exception):
