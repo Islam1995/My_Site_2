4f6435ac656393e8699b2f0fea5e68720dd0a5dd
Islam Elshaikh <islamellshaikh22@gmail.com>
2025-10-11 17:54:50 +0200

    add comment model

--- blog/models.py
+++ blog/models.py
@@ -27,3 +27,10 @@ class Post(models.Model):
     content = models.TextField(validators=[MinLengthValidator(10)])
     author = models.ForeignKey(Author, null=True ,on_delete=models.SET_NULL, related_name='posts')
     tag = models.ManyToManyField(Tag)
+
+
+class Comment(models.Model):
+    user_name = models.CharField(max_length=100)
+    user_email = models.EmailField(max_length=100)
+    text = models.TextField(max_length=400)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
