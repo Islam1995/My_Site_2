9edbb9649176ff376ca006e7defcef29fc19a6dd
Islam Elshaikh <islamellshaikh22@gmail.com>
2025-10-11 03:19:23 +0200

    finish all model and image serve

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
+from .models import Post
 # Create your views here.
 
 def starting_page(request):
-    return render(request,'blog/index.html')
+    all_posts = Post.objects.all().order_by('-date')[:3]
+    return render(request,'blog/index.html',{
+        'posts':all_posts
+    })
 
 def posts(request):
-    return render(request,'blog/all-posts.html')
+    all_posts = Post.objects.all().order_by('-date')
+    return render(request,'blog/all-posts.html',{'posts':all_posts})
 
 def single_post(request, slug):
-    return render(request, 'blog/post-detail.html')
+    post = get_object_or_404(Post, slug = slug)
+    return render(request, 'blog/post-detail.html',{'post':post, 'post_tages':post.tag.all()})
 
 
 # def custom_404(request, exception):
